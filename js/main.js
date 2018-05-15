cd78ca765a82c56210753dbd2ef7e08184282010
Lam Pham <completejavascript.super@gmail.com>
2018-05-15 08:58:43 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,119 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let channels = ["freecodecamp", "javascriptcodingguru", "failarmy"];
-  let cors = 'https://cors-anywhere.herokuapp.com/';
+$(document).ready(() => {
+  const FAVORITE_TWITCHTV_CHANNELS = "favorite-twitchtv-channels";
+  
+  let default_channels = {
+    "freecodecamp" : "freecodecamp",
+    "funfunfunction" : "funfunfunction",
+    "noopkat" : "noopkat",
+    "ferossity" : "ferossity",
+    "kentcdodds" : "kentcdodds",
+    "radicalfishgames" : "radicalfishgames",
+    "scinos" : "scinos",
+    "rthor" : "rthor",
+    "simalexan" : "simalexan",
+    "failarmy" : "failarmy"
+  };
+  
+  let $container = $(".channels");
+  let $btnAdd = $(".add button");
+  let $inpName = $(".add input[type=text]");
+  let $form = $(".add form");
+  let $options = $(".filter .option");
+  let $refresh = $("header .refresh");
 
-  let _container = document.querySelector(".channels");
-
-  channels.forEach(channel => getChannelInfo(channel));
+  let storageHandler = new AppStorage("localStorage");
+  let channels = getChannels() || default_channels;
+  setChannels(channels);
+  render(channels);
+  
+  $btnAdd.on("click", (event) => {
+    event.preventDefault();
+    let channel = $inpName.val().toLowerCase();
+    $form[0].reset();
+    
+    if (!channels[channel]) {
+      channels[channel] = channel;
+      setChannels(channels);
+      getChannelInfo(channel); 
+    }
+    else alert(`Channel '${channel}' already exists!`);
+  });
+  
+  $options.on("click", (event) => {
+    let $target = $(event.target);
+    
+    if ($target.hasClass("active") == false) {
+      $target.siblings().removeClass("active");
+      $target.addClass("active");
+      
+      if ($target.hasClass("all")) filter("all");
+      else if ($target.hasClass("online")) filter("online");
+      else if ($target.hasClass("offline")) filter("offline");
+    }
+  });
+  
+  $refresh.on("click", (event) => {
+    channels = default_channels;
+    setChannels(channels);
+    refreshPage();
+  });
+  
+  function refreshPage(){
+    window.location.href = window.location.href;
+  }
+  
+  function render(channels) {
+    for (let key in channels) {
+      getChannelInfo(key);
+    }
+  }
+  
+  function filter(state) {
+    if(state == 'all') {
+      $container.find(`.item`).removeClass("hidden");
+    }
+    else if(state == 'online' || state == 'offline') {
+      $container.find('.item').addClass("hidden");
+      $container.find(`.item.${state}`).removeClass("hidden");
+    }
+  }
 
   function getChannelInfo(channel) {
-    fetch(makeURL("channels", channel))
-      .then(res => res.json())
-      .then(data => onGetChannelInfoSuccess(channel, data))
-      .catch(error => console.log(error));
+    $.getJSON(makeURL("channels", channel))
+    .done(data => onGetChannelInfoSuccess(channel, data))
+    .fail(error => console.log(error));
   }
 
   function onGetChannelInfoSuccess(channel, data) {
-    let tmpl = itemTemplate(data);
-    let frag = document.createRange().createContextualFragment(tmpl);
-    _container.appendChild(frag);
-
-    getStreamInfo(channel);
+    if (!data.error) {
+      if (data.status === null) data.status = "Offline";
+      
+      let tmpl = itemTemplate(data);
+      $container.append($(tmpl));
+      $container.find(`.item[name="${channel}"] .remove`).on("click", onBtnRemoveClicked);
+      
+      getStreamInfo(channel);
+    }
+    else {
+      delete channels[channel];
+      setChannels(channels);
+      alert(`${data.status} - ${data.message}`);
+    }
+  }
+  
+  function onBtnRemoveClicked(event) {
+    let $item = $(event.target).closest(".item");
+    let name = $item.attr("name");
+    
+    delete channels[name];
+    setChannels(channels);
+    $item.remove();
   }
 
   function getStreamInfo(channel) {
-    fetch(makeURL("streams", channel))
-      .then(res => res.json())
-      .then(data => onGetStreamInfoSuccess(channel, data))
-      .catch(error => console.log(error));
+    $.getJSON(makeURL("streams", channel))
+      .done(data => onGetStreamInfoSuccess(channel, data))
+      .fail(error => console.log(error));
   }
 
   function onGetStreamInfoSuccess(channel, data) {
@@ -38,24 +126,40 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function updateChannelState(channel, state) {
-    let item = _container.querySelector(`.item[name="${channel}"]`);
-    item.classList.add(state);
+    let $item = $container.find(`.item[name="${channel}"]`);
+    $item.addClass(state);
   }
 
   function makeURL(type, name) {
-    return `${cors}https://wind-bow.gomix.me/twitch-api/${type}/${name}?`;
+    return `https://wind-bow.gomix.me/twitch-api/${type}/${name}?callback=?`;
   };
+  
+  function setChannels(channels) {
+    storageHandler.set(FAVORITE_TWITCHTV_CHANNELS, JSON.stringify(channels));
+  }
+  
+  function getChannels() {
+    let channels = storageHandler.get(FAVORITE_TWITCHTV_CHANNELS);
+    if (channels) return JSON.parse(channels);
+  }
 
   function itemTemplate(data) {
     return `
       <div class="item box my-transition" name="${data.name}">
-        <div class="logo-name-wrapper">
-          <div class="logo">
-            <img src="${data.logo}" alt="${data.display_name}'s log" width="80" height="80">
+        <div class="main">
+          <div class="logo-name-wrapper">
+            <div class="logo">
+              <a href="${data.url}" target="_blank">
+                <img src="${data.logo}" alt="${data.display_name}'s log">
+              </a>
+            </div>
+            <div class="name">
+              <a href="${data.url}" target="_blank">${data.display_name}</a>
+            </div>
           </div>
-          <div class="name"><a href="${data.url}" target="_blank">${data.display_name}</a></div>
+          <div class="status">${data.status}</div>
         </div>
-        <div class="status">${data.status}</div>
+        <i class="fa fa-remove remove my-transition"></i>
       </div>
     `;
   }
